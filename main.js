fb7cc8cc7a3a36dea11600e6b738c3c701f38e88
Laasya ^_^~ <81613731+Laasya-tech@users.noreply.github.com>
2021-10-06 19:43:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+noseX=0;
+noseY=0;
+
+
+function preload()
+{
+  Lipstick_image =  loadImage('https://i.postimg.cc/PxFvYgkv/l1.png')
+  Mustache_image =  loadImage('https://i.postimg.cc/3x3QzSGq/m.png')
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if (results.length > 0)
+    { 
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
+    
+}
+
+function draw()
+{
+   image(video, 0, 0, 300, 300);
+   image(Mustache_image, noseX-15 , noseY, 30, 30);
+   image(Lipstick_image, noseX-15 , noseY+20, 30, 30);
+}
+
+function take_snapshot()
+{
+    save('myFilterImage.png');
+}
